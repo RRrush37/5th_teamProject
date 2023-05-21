78e262bf7409158eeaba0c8781a4bf3308733355
RRrush37 <riverstorm1105@gmail.com>
2023-05-21 20:03:01 +0800

    js

--- JS/index/common.js
+++ JS/index/common.js
@@ -1,14 +1,4 @@
 $(() => {
-  let app = "40%";
-  $(".fillBGC").each(function () {
-    var width = $(this).siblings("div").text();
-    width = isNaN(width) ? "0" : width;
-    $(this).css({
-      width: width + "%",
-      backgroundColor: $(".abilityVal").eq(0).css("background-color"),
-    });
-  });
-
   $.ajax({
     url: "php/getAttribute.php",
     type: "POST",
@@ -18,9 +8,22 @@ $(() => {
       // 在此處處理成功的情況
       if (response === -1) {
         alert("帳號或密碼錯誤");
+        $(".abilityVal").html("-");
       } else {
-        location.href = "index_map.html";
+        // location.href = "index_map.html";
+        $("#vitVal").html(response["strength"]);
+        $("#chrVal").html(response["charm"]);
+        $("#intVal").html(response["intelligence"]);
+        $("#lukVal").html(response["luck"]);
       }
+      $(".fillBGC").each(function () {
+        var width = $(this).siblings("div").text();
+        width = isNaN(width) ? "0" : width;
+        $(this).css({
+          width: width + "%",
+          backgroundColor: $(".abilityVal").eq(0).css("background-color"),
+        });
+      });
     },
     error: function (xhr, status, error) {
       // 在此處處理錯誤情況
--- php/getAttribute.php
+++ php/getAttribute.php
@@ -1,4 +1,7 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require("checkIfLogin.php");
 $isLogin = checkIfLogin();
 
@@ -10,8 +13,9 @@ if ($isLogin) {
     $statement->bindValue(1, $_SESSION["ID"]);
     $statement->execute();
 
-    // $data = $statement->fetchAll();
-    // $data = $data[0];
-    // echo json_encode($data);
-    echo 1;
+    $data = $statement->fetchAll();
+    $data = $data[0];
+    echo json_encode($data);
+} else {
+    echo -1;
 }
