481cfe5d451836e853c13b4d2d330d27c2f1771c
JoyceChou914 <e451745172001@gmail.com>
2023-06-05 18:06:19 +0800

    18:00  會員個人資料頁面js修正、儲值紀錄的css修改，頁面確認完成

--- JS/diary/diary_edit.js
+++ JS/diary/diary_edit.js
@@ -1,43 +1,43 @@
-// $(()=>{
-//     $.ajax({
-//         url:"php/getUserData.php",
-//         datatype:"json",
-//         method:"post",
-//         data:{},
-//         success:(response)=>{
-//             if(response == -1){
-//                 alert("請先登入")
-//             }
-//             else{
-//                 response = JSON.parse(response)
-//                 response = response[0]
-//                 if(response["gender"]==0){
-//                     //選女
-//                 }
-//                 else{
-//                     //選男
-//                 }
-//                 $("#hight").val(response["height"])
-//                 $("#weight").val(response["weight"])
-//                 $("#constellation").val(response["starSign"])
-//                 $("#age").val(response["age"])
-//                 $("#birthday").val(response["birthday"])
-//                 $("#interest").val(response["interest"])
-//                 $("#soubriquet").val(response["memberName"])
-//                 $("#myselfsign").val(response["personalSign"])
-//                 $("#email").val(response["email"])
-//                 $("#username").html(response["memberName"])
-//                 $("#idname").html(response["memberID"])
-//                 $("#sign").html(response["personalSign"])
+$(()=>{
+    $.ajax({
+        url:"php/getUserData.php",
+        datatype:"json",
+        method:"post",
+        data:{},
+        success:(response)=>{
+            if(response == -1){
+                alert("請先登入")
+            }
+            else{
+                response = JSON.parse(response)
+                response = response[0]
+                if(response["gender"]==0){
+                    //選女
+                }
+                else{
+                    //選男
+                }
+                $("#hight").val(response["height"])
+                $("#weight").val(response["weight"])
+                $("#constellation").val(response["starSign"])
+                $("#age").val(response["age"])
+                $("#birthday").val(response["birthday"])
+                $("#interest").val(response["interest"])
+                $("#soubriquet").val(response["memberName"])
+                $("#myselfsign").val(response["personalSign"])
+                $("#email").val(response["email"])
+                $("#username").html(response["memberName"])
+                $("#idname").html(response["memberID"])
+                $("#sign").html(response["personalSign"])
                 
                 
-//             }
-//         },
-//         error:(xhr, status, error)=>{
-//             alert("error: "+error)
-//         }
-//     })
-// })
+            }
+        },
+        error:(xhr, status, error)=>{
+            alert("error: "+error)
+        }
+    })
+})
 
 
 
@@ -57,109 +57,138 @@
         
 
 
+        //修左右的變數!!!
         //type=text，設定身高的值,把input的值丟回去aaa
-        var input = document.getElementById('hight');
-        let aaaa = input.value;
+        var height = document.getElementById('hight');
+        console.log(height);
+        let aaaa = height.value;
 
         //興趣
-        var input = document.getElementById('interest');
-        let bbbb = input.value;
+        var interest = document.getElementById('interest');
+        let bbbb = interest.value;
 
         //信箱
-        var input = document.getElementById('email');
-        let cccc = input.value;
+        var email = document.getElementById('email');
+        let cccc = email.value;
 
         //暱稱
-        var input = document.getElementById('soubriquet');
-        let dddd = input.value;
+        var soubriquet = document.getElementById('soubriquet');
+        let dddd = soubriquet.value;
 
         //年齡
-        var input = document.getElementById('age');
-        let eeeee = input.value;
+        var age = document.getElementById('age');
+        let eeeee = age.value;
 
         //星座
-        var input = document.getElementById('constellation');
-        let ffff = input.value;
+        var constellation = document.getElementById('constellation');
+        let ffff = constellation.value;
 
         //生日
-        var input = document.getElementById('birthday');
-        let gggg = input.value;
+        var birthday = document.getElementById('birthday');
+        let gggg = birthday.value;
 
         //個性簽名
-        var input = document.getElementById('myselfsign');
-        let hhhh = input.value;
+        var myselfsign = document.getElementById('myselfsign');
+        let hhhh = myselfsign.value;
+
+        //體重
+        var weight = document.getElementById('weight');
+        let iiii = weight.value;
 
 
-
-
-
-
-
-
-
-        //判斷身高填寫欄位是否為數字
-        //判斷身高要三個數字
-
-            // var input = document.getElementById('hight');
-
-            // var inpur_text=^\d+$;
-            
-            input.addEventListener("keypress", function(e){
+            //身高限制要是數字
+            // let height = document.getElementById("hight");
+            //用keyup事件
+            height.addEventListener("change", function(e){
                 // alert(ka);^\d+$ 
-                console.log(input);
-                if (inpur_text){ }
-                
-                // if else
-                //console.log("觸發了 keyup 事件, 輸入的值: " + this.value);
-                //key code
+                const str = height.value.toString();
+                const isNumber = Number(str);
+                // console.log();
+                if(isNaN(height.value)){
+                    alert("身高欄位請輸入數字唷");
+                }
             });
 
 
 
-            // var text_input = document.getElementById("text_input");
+            //年齡
+            // let age = document.getElementById("age");
+            age.addEventListener("keyup",function(e){
+                const agenumber = age.value.toString();
+                const isNumber = Number(agenumber);
+                if(isNaN(age.value)){
+                    alert("年齡欄位請輸入數字唷");
+                }
+            });
 
-            //     text_input.addEventListener("keydown", function(e){
-            //     //e.preventDefault();
-            //     console.log(e.which);
-                
-            //     //console.log(e); // 觀察 e
-            //     //console.log("觸發了 keydown 事件, key code: " + e.which);
-                
-            //     //console.log(e.which);
-            //     if( (e.which >= 48 && e.which <= 57) || e.which == 8 ){ // 8 是刪除鍵
-                
-            //     }else{
-            //         e.preventDefault(); // 停止預設行為(在欄位上出現所打的文字)
+
+            //體重
+            weight.addEventListener("keyup",function(e){
+                const weightnumber = weight.value.toString();
+                const isNumber = Number(weightnumber);
+                if(isNaN(weight.value)){
+                    alert("體重欄位請輸入數字唷");
+                }
+            });
+
+
+
+
+
+            
+            // age.addEventListener("keyup",function(e){
+            //     const str = age.value.toString();
+            //     const isNumber = Number(str);
+            //     if(isNaN(age.value)){
+            //         alert("請輸入數字");
             //     }
+            // });
+
+
+
+
+            //信箱驗證
+            email.addEventListener("blur",function(){
+                if(email==''){
+                        alert('請輸入電子信箱');
+                        document.getElementById('email').focus();
+                        return false;
+                }else{
+                    var emailRule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+                    var str = email.value ;
+                    if (str.search(emailRule)!= -1){
+                        // document.getElementById('email').focus();
+                        // document.getElementById('email').select();
+                        // return false;
+                    
+                    } else {
+                        alert('電子信箱格式錯誤');
+                    }
+                }
+            });
 
 
 
 
 
-        
 
-
-
-        
-        // console.log(confirmBtn);
         // // 隱藏確認修改和取消修改按鈕
-
-
         confirmBtn.style.display = 'none';
         cancelBtn.style.display = 'none';
         
+        //編輯按鈕
         editBtn.addEventListener('click', function() {
             var fields = document.querySelectorAll('.introduce_right input, .introduce_left input');  // 選取欄位
             
-            aaaa = input.value;
-            bbbb = input.value;
-            cccc = input.value;
-            dddd = input.value;
-            eeee = input.value;
-            ffff = input.value;
-            gggg = input.value;
-            hhhh = input.value;
-
+            aaaa = hight.value;
+            bbbb = interest.value;
+            cccc = email.value;
+            dddd = soubriquet.value;
+            eeee = age.value;
+            ffff = constellation.value;
+            gggg = birthday.value;
+            hhhh = myselfsign.value;
+            iiii = weight.value;
 
 
             // 將欄位的 disabled 屬性設定為 false
@@ -173,24 +202,19 @@
         });
 
 
-        
-        
-
-
-
             //按下編輯按鈕後,選取introduce_right 和 introduce_left 中的所有input欄位都可以進行編輯,
             //使用js中querySelectorAll選取這些欄位,將他們的欄位選取,將他們的disable屬性設定false
             var editBtn = document.querySelector('.editBtn');  // 選取編輯按鈕
 
             editBtn.addEventListener('click', function() {
-            
+                editBtn.style.display = 'none';
             var fields = document.querySelectorAll('.introduce_right input, .introduce_left input');  // 選取欄位
-
+            
                 // 將欄位的 disabled 屬性設定為 false
                 fields.forEach(function(field) {
                     field.disabled = false;
                 });
-                
+            
             });
             
 
@@ -200,24 +224,19 @@
             var cancelBtn = document.querySelector('.cancel');  // 選取取消修改按鈕
 
             cancelBtn.addEventListener('click', function() {
+                editBtn.style.display = 'inline-block';
                 var fields = document.querySelectorAll('.introduce_right input, .introduce_left input');  // 選取欄位
                 
 
-                input.value= aaaa;
-                input.value= bbbb;
-                input.value= cccc;
-                input.value= dddd;
-                input.value= eeee;
-                input.value= ffff;
-                input.value= gggg;
-                input.value= hhhh;
-
-                //if else(點選sex變數時,儲存格就會進行顯示)
-                // var userInput = document.querySelector('input[name="sex"]:checked');
-                // let sex = userInput.value;
-                // console.log(sex);
-                // console.log(userInput.value);
-
+                hight.value= aaaa;
+                interest.value= bbbb;
+                email.value= cccc;
+                soubriquet.value= dddd;
+                age.value= eeee;
+                constellation.value = ffff;
+                birthday.value = gggg;
+                myselfsign.value= hhhh;
+                weight.value = iiii;
 
                 if ( sex == "m" ) {
                     // userInput.checked = false
@@ -234,144 +253,125 @@
                 });
                 confirmBtn.style.display = 'none';
                 cancelBtn.style.display = 'none';
+            
             });
             
 
-
+            //確認按鈕
             //按下確認修改按鈕後,就無法進行編輯動作
             //field跟fields差異
-            var confirmBtn = document.querySelector('.confirm');  // 選取取消修改按鈕
-            
+            //所有條件格式:(如果不ok的部分,要藉由css顯示紅框)
+                    //1.身高限2~3位數
+                    //2.年齡限1~3位數
+                    //3.體重限1~3位數
+                    //4.星座為下拉式選單
+                    //5.信箱:信箱格式
+
+            //綁定確認修改時 (在確認格式跟條件)               
+            //     if(str.length != 3 ){
+            //         alert("請輸入三位數");
+            //     }
+
+            // var height = document.getElementById('hight');
+            // console.log(height);
+            // let aaaa = height.value;
+
+            var confirmBtn = document.querySelector('.confirm');  // 選取確認修改按紐
             confirmBtn.addEventListener('click',function(){
-                //身高、體重、年齡須為數字
-                //身高限2~3位數
-                //年齡限1~3位數
-                //體重限1~3位數
-                //星座為下拉式選單
+                const str = height.value.toString();
+                //身高一定要是三位數
+                if(str.length  !=3){
+                    alert("身高欄位請輸入三位數");
+
+                    height.focus();
+                    height.classList.add("wrong");
+                    return; // 如果身高不是3位數，停止後續操作
+                }else{
+                    height.classList.remove("wrong")
+                };
+
+                // 年齡一定要是二~三位數????沒有成功
+                const agenumber = age.value.toString();
+                console.log(agenumber);
+                if (agenumber.length > 3){
+                    alert("年齡欄位請輸入二到三位數");
+                    
+                    age.focus();//加上錯誤class ,如果錯誤的話加上ouline之類的
+                    age.classList.add("wrong");
+                    return;
+                } else {
+                    age.classList.remove("wrong");
+                }
+
+                
+                const weightnumber = weight.value.toString();
+                //身高一定要是三位數
+                if(weightnumber.length  > 3){
+                    alert("體重欄位請輸入小於等於三位數");
+                    
+                    weight.focus();
+                    weight.classList.add("wrong");
+                    return; // 如果體重不是3位數，停止後續操作
+                } else {
+                    weight.classList.remove("wrong");
+                }
+
+                // weight.addEventListener("keyup",function(e){
+                //     const weightnumber = weight.value.toString();
+                //     const isNumber = Number(weightnumber);
+                //     if(isNaN(weight.value)){
+                //         alert("體重欄位請輸入數字唷");
+                //     }
+                // });
+                
+
+
 
                 var fields = document.querySelectorAll('.introduce_right input, .introduce_left input'); 
-
-                userInput.value;
-                input.value;
-
-                fields.forEach(function(field){
+                    fields.forEach(function(field){
                     field.disabled = true;
+                        
+                        confirmBtn.style.display = 'none';
+                        cancelBtn.style.display = 'none';
+                        editBtn.style.display = 'inline-block';
+                });
+
+
+
+
+
+
+                $.ajax({
+                    url:"php/updateMemberDetail.php",
+                    method:"post",
+                    datatype:"json",
+                    //傳送的資料值
+                    //data傳送到php
+                    data:{
+                        //把height的val設置在height
+                        hight:$("#hight").val(),
+                        weight:$("#weight").val(),
+                        constellation: $("#constellation").val(),
+                        introduce_right:$("#introduce_right").val(),
+                        birthday:$("#birthday").val(),
+                        interest:$("#interest").val(),
+                        soubriquet:$("#soubriquet").val(),
+                        myselfsign:$("#myselfsign").val(),
+                        email:$("#email").val(),
+                    },
+                    //傳送成功的話
+                    success:(response)=>{
+                        if(response ==-1)
+                            alert( "請先登入")
+                        else if(response > 0)
+                            alert("修改成功")
+                        else
+                            alert("修改失敗")
+                    },
+                    //傳送失敗
+                    error:(xhr, status, error) =>{
+                        alert("error: " +error)
+                    }
+                })
+
             });
-                confirmBtn.style.display = 'none';
-                cancelBtn.style.display = 'none';
-
-
-
-
-
-                // $.ajax({
-                //     url:"php/updateMemberDetail.php",
-                //     method:"post",
-                //     datatype:"json",
-                //     //傳送的資料值
-                //     //data傳送到php
-                //     data:{
-                //         //把height的val設置在height
-                //         hight:$("#hight").val(),
-                //         weight:$("#weight").val(),
-                //         constellation: $("#constellation").val(),
-                //         introduce_right:$("#introduce_right").val(),
-                //         birthday:$("#birthday").val(),
-                //         interest:$("#interest").val(),
-                //         soubriquet:$("#soubriquet").val(),
-                //         myselfsign:$("#myselfsign").val(),
-                //         email:$("#email").val(),
-                //     },
-                //     //傳送成功的話
-                //     success:(response)=>{
-                //         if(response ==-1)
-                //             alert( "請先登入")
-                //         else if(response > 0)
-                //             alert("修改成功")
-                //         else
-                //             alert("修改失敗")
-                //     },
-                //     //傳送失敗
-                //     error:(xhr, status, error) =>{
-                //         alert("error: " +error)
-                //     }
-                // })
-
-            });
-
-            //建立參數去儲值資料
-            //傳回後端
-            // var value = element.value;
-
-
-            //type=radio
-            //value 不能在外面
-
-            // var inputElement = document.getElementById('male');
-
-
-
-
-
-
-            //顯示值為new 
-
-
-
-
-
-            //判別數值式子//欄位事件//key in &key up
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    // // 透過 class 取得元素
-    // let edit_lightbox = document.getElementsByClassName("edit_lightbox")[0];
-
-    // // 切換按鈕的部分
-    // let fa_pen_tosquare = document.getElementsByClassName("fa-pen-to-square")[0];
-
-    // fa_pen_tosquare.addEventListener("click", function(){
-    //     edit_lightbox.classList.remove("none");
-    //     // alert("lalala");
-    // });
-
-
-
-    // // 進入取消按鈕時，燈箱不見
-    // let cancel_close = document.getElementsByClassName("cancel")[0];
-    // let send_close = document.getElementsByClassName("send")[0];
-
-    // cancel_close.addEventListener("click", function(){
-
-    //     edit_lightbox.classList.add("none");
-    // });
-
-    // send_close.addEventListener("click", function(){
-
-    //     edit_lightbox.classList.add("none");
-    // });
-
-
-    // edit_lightbox.addEventListener("click", function(){
-    // this.classList.add("none");
-    // });
-
-
-    // // 停止冒泡事件()   
-    // let wrapper1 = document.getElementsByClassName("wrapper1")[0];
-
-    // wrapper1.addEventListener("click", function (e) {
-    //     e.stopPropagation();
-    // });
