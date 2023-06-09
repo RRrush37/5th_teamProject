916e1184ee0807a7b0d91409884e71cb897a18bd
RRrush37 <riverstorm1105@gmail.com>
2023-06-09 17:01:01 +0800

    活動頁留言+發文

--- php/activityComment.php
+++ php/activityComment.php
@@ -1,44 +1,29 @@
 <?php
 // 處理資料
-    // $jsonData = file_get_contents('php://input');
-    // $data = json_decode($jsonData);
-    // $activityID = $data->activityID;
-    // $comment_content = $data->comment_content;
+// $jsonData = file_get_contents('php://input');
+// $data = json_decode($jsonData);
+// $activityID = $data->activityID;
+// $comment_content = $data->comment_content;
 
-// require("checkLogin.php");
-// session_start();
-// if (!checkLogin()) {
+require("checkIfLogin.php");
+session_start();
+if (checkIfLogin()) {
     require("connectSQL.php");
-    $sql = 'INSERT into activityComment(activityID, content,`time`)
-values(?,?,now())';
+    $sql = 'INSERT into activityComment(commentID, activityID, content, `time`)
+values(?, ?,?,now())';
     $statement = $pdo->prepare($sql);
-    // $statement->bindValue(1, $_SESSION["login"]);
-    // $statement->bindValue(1, 2);
+    $statement->bindValue(1, $_SESSION["ID"]);
+    $statement->bindValue(2, (int)$_POST["activityId"]);
+    $statement->bindValue(3, $_POST["commentContent"]);
 
-    $statement->bindValue(1, $activityId);
-    $statement->bindValue(2, $commentContent);
-
-    // var_dump($_POST); 
-    // $statement->bindValue(1, 1);
-    // $statement->bindValue(2, 2);
-    // $statement->bindValue(3, 555);
-    // $statement->bindValue(4, 777);
-    // $statement->bindValue(5, 777);
-    // $statement->bindValue(6, "2023-06-06");
-    // $statement->bindValue(7, "2023-06-06");
-    // $statement->bindValue(8, 1);
-    // $statement->bindValue(9,0);
-    // $statement->bindValue(10,1);
-    // $statement->bindValue(11,0);
-    // $statement->bindValue(12,0);
-
-
+    // $statement->bindValue(1, $_SESSION["ID"]);
+    // $statement->bindValue(2, "20000");
+    // $statement->bindValue(3, "23122");
     $go = $statement->execute();
     if ($go) {
-    echo 1;
+        echo 1;
     } else
-    echo 2;
-// } else {
-//     echo -1;
-// }
-?>
+        echo 2;
+} else {
+    echo -1;
+}
