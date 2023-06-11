0b4194499473a231b334c26407bb3f9f276fecd1
Ling-Chieh <joy79318@gmail.com>
2023-06-11 16:00:34 +0800

    後台資料更新、後台登入密碼限制更新、catch_image.php路徑修改

---
+++ php/back_getMemberData.php
@@ -0,0 +1,16 @@
+<?php 
+    require("checkIfLogin.php");
+    session_start();
+    if(checkIfLogin()){
+        require("connectSQL.php");
+
+        $sql = "select * from memberData where memberID = ?";
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(1, $_POST["memberID"]);
+        $statement->execute();
+        $data = $statement->fetchAll();
+        echo json_encode($data);
+    }
+    else{
+        echo -1;
+    }
