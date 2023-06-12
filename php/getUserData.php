214c651e9ab76c1ed6dd49ebcbd514863dc67d93
RRrush37 <riverstorm1105@gmail.com>
2023-06-12 14:44:34 +0800

    getUserDAta更新

--- php/getUserData.php
+++ php/getUserData.php
@@ -1,17 +1,17 @@
-<?php 
-    require("checkIfLogin.php");
-    session_start();
-    if(checkIfLogin()){
-        require("connectSQL.php");
+<?php
+// require("checkIfLogin.php");
+// session_start();
+// if(checkIfLogin()){
+require("connectSQL.php");
 
-        $sql = "select * from memberData where memberID = ?";
-        $statement = $pdo->prepare($sql);
-        $statement->bindValue(1, $_SESSION["ID"]);
-        $statement->execute();
-    
-        $data = $statement->fetchAll();
-        echo json_encode($data);
-    }
-    else{
-        echo -1;
-    }
+$sql = "select * from memberData where memberID = ?";
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $_SESSION["ID"]);
+$statement->execute();
+
+$data = $statement->fetchAll();
+echo json_encode($data);
+    // }
+    // else{
+    //     echo -1;
+    // }
