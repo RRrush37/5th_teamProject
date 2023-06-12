b59fd0b3f34f872b5e85245c553c66c3c8268f1b
RRrush37 <riverstorm1105@gmail.com>
2023-06-12 14:46:49 +0800

    php改回來

--- php/getUserData.php
+++ php/getUserData.php
@@ -1,17 +1,16 @@
 <?php
-// require("checkIfLogin.php");
-// session_start();
-// if(checkIfLogin()){
-require("connectSQL.php");
+require("checkIfLogin.php");
+session_start();
+if (checkIfLogin()) {
+    require("connectSQL.php");
 
-$sql = "select * from memberData where memberID = ?";
-$statement = $pdo->prepare($sql);
-$statement->bindValue(1, $_SESSION["ID"]);
-$statement->execute();
+    $sql = "select * from memberData where memberID = ?";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(1, $_SESSION["ID"]);
+    $statement->execute();
 
-$data = $statement->fetchAll();
-echo json_encode($data);
-    // }
-    // else{
-    //     echo -1;
-    // }
+    $data = $statement->fetchAll();
+    echo json_encode($data);
+} else {
+    echo -1;
+}
